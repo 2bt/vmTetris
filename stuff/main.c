db0432031b3a24d0395449840e63d7f27d65ccf8
twobit <s8572327@gmail.com>
2017-02-01 15:13:13 +0100

    stuff

--- stuff/main.c
+++ stuff/main.c
@@ -13,7 +13,7 @@ int f(int l) { for (x = 0; l--;) x = x * 80 + b[c++] - 46; return x; }
 int* d() { return a = f(1) < R ? m + x : m + m[x - R]++; }
 int s() { return f(1) < R ? m[x] : m[m[x - R]]; }
 
-enum { END, LD1, LD2, MOV, ADD, SUB, INC, DEC, GET, PUT, RND, SLP, JMP, JPZ, JNZ };
+enum { END, MOV, LD1, LD2, ADD, SUB, INC, DEC, GET, PUT, PUT2, PUT3, RND, SLP, JMP, JPZ, JNZ };
 
 int main(int argc, char** argv) {
 
@@ -23,29 +23,24 @@ int main(int argc, char** argv) {
 	fread(b, 1, sizeof(b), v);
 	fclose(v);
 
-//	srand(time(0));
-	int i = 0;
-
-	srand(1472503431);
+	srand(time(0));
 
 	system("stty cbreak -echo min 0");
 	while ((o = f(1))) {
 		switch (o) {
-		case LD1:
-		case LD2: d(); *a = f(o); break;
-		case MOV: d(); *a = s(); break;
+		case MOV:
+      case LD1:
+		case LD2: d(); *a = o > 1 ? f(o - 1) : s(); break;
 		case ADD: d(); *a += s(); break;
 		case SUB: d(); *a -= s(); break;
 		case INC: ++*d(); break;
 		case DEC: --*d(); break;
 		case GET: *d() = getchar(); break;
-		case PUT: putchar(*a); break;
+		case PUT:
+		case PUT2:
+		case PUT3: putchar(f(o - PUT) ?: *a); break;
 		case RND: *a = rand() % *a; break;
-		case SLP: usleep(20000);
-//			printf("%4d) %03x:", i++, c);
-//			for (x=0;x<R;x++)printf("|%d", m[x]);
-//			printf("\n");
-			break;
+		case SLP: usleep(20000); break;
 		case JMP:
 		case JPZ:
 		case JNZ: f(2); if (o == JMP || !*a ^ (o > JPZ)) c = x; break;
