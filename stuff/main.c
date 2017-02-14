986e8db16cc1edb2d814155032afa6835ad4f34f
twobit <s8572327@gmail.com>
2017-02-14 14:30:31 +0100

    shorten

--- stuff/main.c
+++ stuff/main.c
@@ -13,7 +13,7 @@ int f(int l) { for (x = 0; l--;) x = x * 80 + b[c++] - 46; return x; }
 int* d() { return a = f(1) < R ? m + x : m + m[x - R]++; }
 int s() { return f(1) < R ? m[x] : m[m[x - R]]; }
 
-enum { END, MOV, LD1, LD2, ADD, SUB, INC, DEC, GET, PUT, PT1, PT2, RND, SLP, JMP, JNZ, JZ };
+enum { END, MOV, LD1, LD2, ADD, SUB, INC, DEC, JMP, JNZ, JZ, GET, PUT, PT1, PT2, RND, SLP, };
 
 int main(int argc, char** argv) {
 
@@ -35,15 +35,15 @@ int main(int argc, char** argv) {
 		case SUB: d(); *a -= s(); break;
 		case INC: ++*d(); break;
 		case DEC: --*d(); break;
+		case JMP:
+		case JZ:
+		case JNZ: f(2); if (o == JMP || !*a ^ (o < JZ)) c = x; break;
 		case GET: *d() = getchar(); break;
 		case PUT:
 		case PT1:
 		case PT2: putchar(f(o - PUT) ?: *a); break;
 		case RND: *a = rand() % *a; break;
 		case SLP: usleep(20000); break;
-		case JMP:
-		case JZ:
-		case JNZ: f(2); if (o == JMP || !*a ^ (o < JZ)) c = x; break;
 		}
 	}
 	system("stty sane");
