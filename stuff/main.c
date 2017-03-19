ffdd42c76557b23a81d90fad6ec0e45f0d41c6bf
twobit <s8572327@gmail.com>
2017-03-19 14:13:17 +0100

    good stuff

--- stuff/main.c
+++ stuff/main.c
@@ -3,27 +3,27 @@
 #include <time.h>
 #include <unistd.h>
 
-char b[1<<10];
+char b[1000];
 
 int m[444], c, *a, x;
 
-int f(int l) { for (x = 0; l--;) x = x * 80 + b[c++] - 46; return x; }
+int f() { x = b[c++] - 46; return x; }
 
 int main(int o, int _) {
 	FILE* v = fopen("code", "r");
 	fread(b, 1, sizeof(b), v);
 	fclose(v);
 	system("stty cbreak -echo min 0");
-	for (srand(_); o = f(1);)
-		--o < 24 ? a = &m[f(1) < 20 ? x : m[x - 20]++],
-			*a = !(o / 16) * *a + (o / 8 ? 1 : -1) * (
-			(x = o % 8) < 3 ? x:
-			x < 5 ? f(x - 2):
-			x < 6 ? m[f(1) < 20 ? x : m[x - 20]]:
-			x < 7 ? getchar():
-			rand() % f(1)):
-		o < 27 ? f(2), c = o < 25 || !*a ^ o % 2 ? x : c:
-		o < 30 ? putchar(f(o % 3) ?: *a):
+	for (srand(_); o = f();)
+		--o < 24 ? a = &m[f() < 20 ? x : m[x - 20]++],
+			*a = !(o / 14) * *a + (o / 8 ? 1 : -1) * (
+			(x = o % 7) < 3 ? x:
+			x < 4 ? f():
+			x < 5 ? m[f() < 20 ? x : m[x - 20]]:
+			x < 6 ? getchar():
+			rand() % f()):
+		o < 60 ? f(), c = o < 40 || !*a ^ o / 50 ? x + o % 10 * 80 : c:
+		o < 63 ? putchar(o % 3 ? o % 2 * 80 + f() : *a):
 		usleep(20000);
 	system("stty sane");
 }
@@ -32,4 +32,4 @@ int main(int o, int _) {
 // a - x
 // a + x
 // 0 + x
-// 0, 1, 2, f(1), f(2), s(), getchar(), rand() % f(1)
+// 0, 1, 2, f(), s(), getchar(), rand()%f()
