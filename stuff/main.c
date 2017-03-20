a59e440d2e0a30cd5f6064676ac7a6289b74539e
twobit <s8572327@gmail.com>
2017-03-20 21:31:07 +0100

    improve

--- stuff/main.c
+++ stuff/main.c
@@ -7,7 +7,7 @@ char b[1000];
 
 int m[444], c, x;
 
-int f() { return x = b[c++] - 45; }
+int f() { return x = b[c++] - 46; }
 
 int main(int o, int a) {
 	FILE* v = fopen("code", "r");
@@ -16,16 +16,15 @@ int main(int o, int a) {
 
 	system("stty cbreak -echo min 0");
 	for (srand(a); o = f();)
-		--o < 42 ? m[a = o % 2 ? f() < 20 ? x : m[x - 20]++ : m[ 4 ]++] *= o < 28,
+		o < 42 ? m[a = o % 2 ? f() < 20 ? x : m[x - 20]++ : m[4]++] *= o < 28,
 			a = m[a] += (o > 13 ?: -1) * (
 			(x = o / 2 % 7) < 3 ? x:
 			x < 4 ? f():
 			x < 5 ? m[f() < 20 ? x : m[x - 20]]:
 			x < 6 ? getchar():
 			rand() % f()):
-
 		o < 45 ? putchar(o % 3 ? o % 2 * 74 + f() : a):
-		o < 72 ? f(), c = o < 54 | !a ^ o / 63 ? x + o % 9 * 74 : c:
+		o < 63 ? f(), c = !a ^ o / 54 ? x + o % 9 * 74 : c:
 		usleep(20000);
 	system("stty sane");
 }
@@ -34,4 +33,5 @@ int main(int o, int a) {
 // a - x
 // a + x
 // 0 + x
+
 // 0, 1, 2, f(), s(), getchar(), rand()%f()
