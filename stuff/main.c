9f7d998aa65b3c593b9017b7847699ac64f67a6a
twobit <s8572327@gmail.com>
2017-03-20 23:01:12 +0100

    shorten

--- stuff/main.c
+++ stuff/main.c
@@ -16,19 +16,21 @@ int main(int o, int a) {
 
 	system("stty cbreak -echo min 0");
 	for (srand(a); o = f();)
-		o < 42 ? m[a = o % 2 ? f() < 20 ? x : m[x - 20]++ : m[4]++] *= o < 28,
+		o < 42 ? m[a = o % 2 ? f() : m[4]++] *= o < 28,
 			a = m[a] += (o > 13 ?: -1) * (
 			(x = o / 2 % 7) < 3 ? x:
 			x < 4 ? f():
 			x < 5 ? m[f() < 20 ? x : m[x - 20]]:
 			x < 6 ? getchar():
 			rand() % f()):
-		o < 45 ? putchar(o % 3 ? o % 2 * 74 + f() : a):
-		o < 63 ? f(), c = !a ^ o / 54 ? x + o % 9 * 74 : c:
+		o < 45 ? putchar(o % 3 ? o % 2 * 75 + f() : a):
+		o < 63 ? f(), c = !a ^ o / 54 ? x + o % 9 * 75 : c:
 		usleep(20000);
 	system("stty sane");
 }
 
+// f(), m[4]++, 5
+// 7*3*3
 
 // a - x
 // a + x
