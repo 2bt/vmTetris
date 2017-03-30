9c3095205117e8646b7e821445459308f256d6b5
twobit <s8572327@gmail.com>
2017-03-30 12:36:01 +0200

    shorten

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 m[444],c,x;main(o,a){for(system("stty cbreak -echo min 0"),srand(a);o=f();o<42?m
-[a=o%2?f():m[4]++]*=o<28,a=m[a]+=(o>13?:-1)*((x=o/2%7)<3?x:x<4?f():x<5?m[f()<20?
-x:m[x-20]]:x<6?getchar():rand()%f()):o<45?putchar(o%3?o%2*75+f():a):o<63?f(),c=!
-a^o/54?x+o%9*75:c:usleep(20000));system("stty sane");}f(){return x=c++["M@Q?LQ4\
-RC2814d6Q42P;P8L14d@P9NP8LP8P:LP8NLP:P8A4LPDP9P9NL14d^LPDLP9NP7?2LPDNP7NLPCL?2P\
-9NLLPCP9NP9O4?2LP:NP9P814eFO4LLP7LP:P914eVQ2DQ9DP5C2819eiQ9:P519euZIY>Z`Zx_^S;:\
-S=<KA1@]YS49U9?9fF54Jb754UfaC:9MAS@?14fg?:14fm1:54bg/5<7^/C<:O5Q2DQ9EQ8:S4F34gF\
-34J12S6:C6:762^pS7>E7<S6:Q31E6K762^pE62?713g]_KA4_GQ6H76:h9S6<76=b7=A_?M5S<=S:;\
-g1R512?218g:1219g7M6756jMQ:HW35C3LQ69S>3E>316hmW<1S6<E<6E<6Q2DQ9DS4F?214b719i;Q\
-2ZQ9C?2Q88Q48M676Fi_14?218iV=4jIS82K247`n12B8S28=.j41?S82S42549RH3254Ej7S2819iN\
-ZIY>ZiZvQ2ZQ9CQ8:ZIY>ZbS4FC4^XYPZNZN?218j^12Z819j[m]:ZIY>YPY/YLYQYHYVZhZNQ21S6F\
-kW12kN?212B^X12kYZ8."]-46;}///////////////////////////////////////////////twobit
+[a=o%2?f():m[6]++]*=o<28,a=m[a]+=(o>13?:-1)*((x=o/2%7)<3?x:x<4?f():x<5?m[f()<23?
+x:m[x-23]]:x<6?getchar():rand()%f()):o<45?putchar(o%3?o%2*75+f():a):o<63?f(),c=!
+a^o/54?x+o%9*75:c:usleep(20000));system("stty sane");}f(){return x=c++["NCR7M@B\
+D5;27e4R73Q<Q9M27e@Q:OQ9MQ9Q;MQ9OMQ;Q9B7MQEQ:Q:OM27e^MQEMQ:OQ8@5MQEOQ8OMQDM@5Q:\
+OMMQDQ:OQ:P7@5MQ;OQ:Q927fFP7MMQ8MQ;Q:27fVR5ER<EQ6D592<fiR<;Q62<fu[JZ?[a[y`ST>=T\
+@?LD2C^YT7<V<@<gF67Kbw67VgaD=:NDTCB27gg@=27gm2=67ch/6?8_/D?;P8R5ER<FR;;T7L47hF4\
+7K25T9=D9;895_pT:AF:?T9=R62F9Q895_pF95@:26h]`CB7`?R9I89=bw>D`7N8T?@T=>h1S825@52\
+;h:252<h728kBR=IX66D6MR9:TA6FA629ibX?2T9?F?9F?9R5ER<ET7L@527bw2<j0R5[R<D@5R;9R7\
+9N989LjT27@52;jK>7k>T;5L558ac25C9T5;>/jt2BT;5T7567:SN4567FjwT5;2<jC[JZ?[j[wR5[R\
+<DR;;[JZ?[cT7LD7_YZQ[O[O@52;kS25[92<kPn^:[JZ?ZQZ0ZMZRZIZW[i[OR53T9LlL25lC@525C_\
+Y25lN[9/"]-47;}///////////////////////////////////////////////////////////twobit
--- stuff/main.c
+++ stuff/main.c
@@ -4,18 +4,18 @@
 #include <unistd.h>
 char b[1000];
 int m[444], c, x;
-int f() { return x = b[c++] - 46; }
+int f() { return x = b[c++] - 47; }
 int main(int o, int a) {
 	FILE* v = fopen("code", "r");
 	fread(b, 1, sizeof(b), v);
 	fclose(v);
 	system("stty cbreak -echo min 0");
 	for (srand(a); o = f();)
-		o < 42 ? m[a = o % 2 ? f() : m[4]++] *= o < 28,
+		o < 42 ? m[a = o % 2 ? f() : m[6]++] *= o < 28,
 			a = m[a] += (o > 13 ?: -1) * (
 			(x = o / 2 % 7) < 3 ? x:
 			x < 4 ? f():
-			x < 5 ? m[f() < 20 ? x : m[x - 20]]:
+			x < 5 ? m[f() < 23 ? x : m[x - 23]]:
 			x < 6 ? getchar():
 			rand() % f()):
 		o < 45 ? putchar(o % 3 ? o % 2 * 75 + f() : a):
