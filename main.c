417aff2aa385bf57f369af006a5ff782f8ec77fb
twobit <s8572327@gmail.com>
2017-03-30 15:37:43 +0200

    shorten

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-m[444],c,x;main(o,a){for(system("stty cbreak -echo min 0"),srand(a);o=f();o<42?m
-[a=o%2?f():m[6]++]*=o<28,a=m[a]+=(o>13?:-1)*((x=o/2%7)<3?x:x<4?f():x<5?m[f()<23?
-x:m[x-23]]:x<6?getchar():rand()%f()):o<45?putchar(o%3?o%2*75+f():a):o<63?f(),c=!
-a^o/54?x+o%9*75:c:usleep(20000));system("stty sane");}f(){return x=c++["NCR7M@B\
-D5;27e4R73Q<Q9M27e@Q:OQ9MQ9Q;MQ9OMQ;Q9B7MQEQ:Q:OM27e^MQEMQ:OQ8@5MQEOQ8OMQDM@5Q:\
-OMMQDQ:OQ:P7@5MQ;OQ:Q927fFP7MMQ8MQ;Q:27fVR5ER<EQ6D592<fiR<;Q62<fu[JZ?[a[y`ST>=T\
-@?LD2C^YT7<V<@<gF67Kbw67VgaD=:NDTCB27gg@=27gm2=67ch/6?8_/D?;P8R5ER<FR;;T7L47hF4\
-7K25T9=D9;895_pT:AF:?T9=R62F9Q895_pF95@:26h]`CB7`?R9I89=bw>D`7N8T?@T=>h1S825@52\
-;h:252<h728kBR=IX66D6MR9:TA6FA629ibX?2T9?F?9F?9R5ER<ET7L@527bw2<j0R5[R<D@5R;9R7\
-9N989LjT27@52;jK>7k>T;5L558ac25C9T5;>/jt2BT;5T7567:SN4567FjwT5;2<jC[JZ?[j[wR5[R\
-<DR;;[JZ?[cT7LD7_YZQ[O[O@52;kS25[92<kPn^:[JZ?ZQZ0ZMZRZIZW[i[OR53T9LlL25lC@525C_\
-Y25lN[9/"]-47;}///////////////////////////////////////////////////////////twobit
+m[444],c,x;main(o,a){for(system("stty cbreak -echo min 0"),srand(a);o=f();o<42?a
+=(x=o/2%7)<3?x:x<4?f():x<5?m[f()<23?x:m[x-23]]:x<6?getchar():rand()%f(),m[x=o%2?
+f():m[6]++]*=o<28,a=m[x]+=o>13?a:-a:o<45?putchar(o%3?o%2*75+f():a):o<63?f(),c=!a
+^o/54?x+o%9*75:c:usleep(20000));system("stty sane");}f(){return x=c++["NCRM7@BD\
+;527e4R37Q<Q9M27e@Q:OQ9MQ9Q;MQ9OMQ;Q9B7MQEQ:Q:OM27e^MQEMQ:OQ8@5MQEOQ8OMQDM@5Q:O\
+MMQDQ:OQ:P7@5MQ;OQ:Q927fFP7MMQ8MQ;Q:27fVRE5RE<Q6D952<fiR;<Q62<fu[JZ?[a[y`ST=>T?\
+@LD2C^YT<7V<@<gF6K7bw6V7gaD:=NDTBC27gg@=27gm2=6c7h/68?_/D;?P8RE5RF<R;;TL747hF47\
+K25T=9D;9859_pTA:F?:T=9R26FQ9859_pF59@:26h]`CB7`?RI98=9bw>D`7N8T@?T>=h1S825@52;\
+h:252<h728kBRI=X66DM6R:9T6AF6A29ibX2?T?9F9?F9?RE5RE<TL7@527bw2<j0R[5RD<@5R9;R97\
+N98L9jT27@52;jK>7k>T5;L558ac25C9T;5>/jt2BT5;T576:7SN456F7jwT;52<jC[JZ?[j[wR[5RD\
+<R;;[JZ?[cTL7D_7YZQ[O[O@52;kS25[92<kPn^:[JZ?ZQZ0ZMZRZIZW[i[OR35TL9lL25lC@525C_Y\
+25lN[9/"]-47;}////////////////////////////////////////////////////////////twobit
--- stuff/main.c
+++ stuff/main.c
@@ -11,13 +11,14 @@ int main(int o, int a) {
 	fclose(v);
 	system("stty cbreak -echo min 0");
 	for (srand(a); o = f();)
-		o < 42 ? m[a = o % 2 ? f() : m[6]++] *= o < 28,
-			a = m[a] += (o > 13 ?: -1) * (
-			(x = o / 2 % 7) < 3 ? x:
-			x < 4 ? f():
-			x < 5 ? m[f() < 23 ? x : m[x - 23]]:
-			x < 6 ? getchar():
-			rand() % f()):
+		o < 42 ?
+			a = (x = o / 2 % 7) < 3 ? x:
+				x < 4 ? f():
+				x < 5 ? m[f() < 23 ? x : m[x - 23]]:
+				x < 6 ? getchar():
+				rand() % f(),
+			m[x = o % 2 ? f() : m[6]++] *= o < 28,
+			a = m[x] += o > 13 ? a : -a:
 		o < 45 ? putchar(o % 3 ? o % 2 * 75 + f() : a):
 		o < 63 ? f(), c = !a ^ o / 54 ? x + o % 9 * 75 : c:
 		usleep(20000);
